d399449ce130667f72ab0958414f0fe81194ab9a
Becca <rebecca.vasil@gmail.com>
2014-10-13 13:29:33 -0600

    Song that  never ends

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get "/verse 2" => "pages#verse_2", as: :verse2
+  get "/verse 3" => "pages#verse_3", as: :verse3
+  get "/verse 4" => "pages#verse_4", as: :verse4
+
 end
